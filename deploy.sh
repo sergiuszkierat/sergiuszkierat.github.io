ce88b9d0e1d601a27ab53b3ad056dda3550ed2db
Travis CI <noreply@travis-ci.org>
2016-01-05 23:07:40 +0000

    Site updated at: 2016-01-05 23:07:40 UTC

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 set -e # exit with nonzero exit code if anything fails
 
+echo 'Creating _deploy.xml...'
 echo "method: git" > _deploy.yml
 echo "git_url: https://${GH_TOKEN}@${GH_REF}" >> _deploy.yml
 
 echo 'Deploying through octopress...'
-octopress deploy -t > /dev/null 2>&1
+octopress deploy > /dev/null 2>&1
 echo 'Done'
