7c1ea3e7159a8af696f969344b4bf1106ae21e0b
Travis CI <build@travisci.org>
2016-01-05 14:45:50 +0000

    Site updated at: 2016-01-05 14:45:50 UTC

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 set -e # exit with nonzero exit code if anything fails
 
-echo "? :" `env | grep GH_TOKEN`
-
 echo "method: git" > _deploy.yml
 echo "git_url: https://${GH_TOKEN}@${GH_REF}" >> _deploy.yml
 
 echo 'Deploying through octopress...'
 octopress deploy -t > /dev/null 2>&1
+echo 'Done'
