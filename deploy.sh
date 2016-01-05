25fcbd722e843cb0b0763453e07e7be1243b664d
Sergiusz Kierat <sergiusz.kierat@gmail.com>
2016-01-05 14:55:10 +0100

    [WIP] Continuous Delivery with Travis CI

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 set -e # exit with nonzero exit code if anything fails
 
-echo "GH_TOKEN : ${GH_TOKEN}"
 echo "? :" `env | grep GH_TOKEN`
 
-echo 'Octopress...'
-octopress deploy
+echo "method: git" > _deploy.yml
+echo "git_url: https://${GH_TOKEN}@${GH_REF}" >> _deploy.yml
+
+echo 'Deploying through octopress...'
+octopress deploy -t
